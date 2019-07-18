7e0f5a0965399fcccc02648ad132c346600c5fc9
scc85 <sarahchristinec85@gmail.com>
2019-07-18 13:18:03 -0700

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const router = require("express").Router();
 
-router.get("*", function (req, res) {
+router.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
 });
 
