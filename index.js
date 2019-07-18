d8bf084fd6dfa73912ef1b998a9ac763042dc072
scc85 <sarahchristinec85@gmail.com>
2019-07-18 14:02:04 -0700

    routes

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/api/form', (req, res) => {
 });
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
